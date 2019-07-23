2eecb0d4c91ffaf86c01a8a192df04d436c717f4
horitomo <ths75149@gmail.com>
2019-07-23 11:41:37 +0900

    [UPD] ストアをインポート

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+// ストアをインポート
+import store from './store' 
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store, // ストア追加
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 });
