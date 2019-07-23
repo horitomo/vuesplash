ed9ee68ba6488341574446843374e3692cbcb667
horitomo <ths75149@gmail.com>
2019-07-23 11:31:38 +0900

    [UPD] loginとregisterのフォーム追加

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -13,7 +13,7 @@
       >Register</li>
     </ul>
     <div class="panel" v-show="tab === 1">
-      <form class="form" @submit.prevent="login">
+      <form class="form"   @submit.prevent="login">
         <label for="login-email">Email</label>
         <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
         <label for="login-password">Password</label>
@@ -22,8 +22,21 @@
           <button type="submit" class="button button--inverse">login</button>
         </div>
       </form>
-  </div>
-  <div class="panel" v-show="tab === 2">Register Form</div>
+    </div>
+    <div class="panel" v-show="tab === 2"><form class="form" @submit.prevent="register">
+      <label for="username">Name</label>
+      <input type="text" class="form__item" id="username" v-model="registerForm.name">
+      <label for="email">Email</label>
+      <input type="text" class="form__item" id="email" v-model="registerForm.email">
+      <label for="password">Password</label>
+      <input type="password" class="form__item" id="password" v-model="registerForm.password">
+      <label for="password-confirmation">Password (confirm)</label>
+      <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">register</button>
+      </div>
+    </form>
+    </div>
   </div>
 </template>
 
@@ -36,11 +49,20 @@ export default {
         email: '',
         password: ''
       },
+       registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
     }
   },
   methods: {
     login () {
       console.log(this.loginForm)
+    },
+    register () {
+      console.log(this.registerForm)
     }
   }
 }
