382530216a37403c988b9ffbf5d871320ce7534f
horitomo <ths75149@gmail.com>
2019-08-13 17:38:24 +0900

    [UPD] ダウンロードルート追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
 
+// 写真ダウンロード
+Route::get('/photos/{photo}/download', 'PhotoController@download');
+
 Route::get('/{any?}', function () {
     return view('index');
 })->where('any','.+');
